531acb8e47fd8f8fb38c72609187ae4c5e8241d4
Ajayram Meena <ajayram9672@gmail.com>
2026-03-28 19:48:54 +0530

    Added app icons, updated configs and assets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.project"
+    namespace = "com.incitrack.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,10 +22,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // This MUST match the package_name in your google-services.json
         applicationId = "com.example.project"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -34,8 +33,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -45,18 +42,7 @@ flutter {
     source = "../.."
 }
 dependencies {
-  // Core library desugaring for Java 8 features
   coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-
-  // Import the Firebase BoM
   implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
-
-
-  // TODO: Add the dependencies for Firebase products you want to use
-  // When using the BoM, don't specify versions in Firebase dependencies
   implementation("com.google.firebase:firebase-analytics")
-
-
-  // Add the dependencies for any other desired Firebase products
-  // https://firebase.google.com/docs/android/setup#available-libraries
 }
